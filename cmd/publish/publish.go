8944692d26d8341818a5be309f349dfb600edb21
Julian Easterling <julian@julianscorner.com>
2026-03-08 17:20:37 -0400

    Refactored the Publish command

---
+++ cmd/publish/publish.go
@@ -0,0 +1,94 @@
+/*
+Copyright © 2026 Julian Easterling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package publish
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/dcjulian29/cert-auth/internal/shared"
+	"github.com/dcjulian29/go-toolbox/color"
+	"github.com/dcjulian29/go-toolbox/filesystem"
+	"github.com/spf13/cobra"
+)
+
+func NewCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "publish",
+		Short: "Publish the certificate authority files suitable for deployment to web",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return shared.IsRootCertificateAuthority()
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dest, _ := cmd.Flags().GetString("destination")
+
+			if filesystem.DirectoryExists(dest) {
+				files, err := os.ReadDir(dest)
+				if err != nil {
+					return err
+				}
+
+				if len(files) > 0 {
+					if f, _ := cmd.Flags().GetBool("force"); f {
+						if err := os.RemoveAll(dest); err != nil {
+							return err
+						}
+					} else {
+						return errors.New("files present in the publish path and Force was not supplied")
+					}
+				}
+			}
+
+			if err := filesystem.EnsureDirectoryExist(dest); err != nil {
+				return err
+			}
+
+			if err := mime_types(filepath.Join(dest, "mime.types")); err != nil {
+				return err
+			}
+
+			settings, err := shared.GetSettings()
+			if err != nil {
+				return err
+			}
+
+			publish_files(settings, dest)
+
+			for _, s := range settings.Subordinates {
+				if err := shared.ValidateSubordinate(s.Id, s.Name); err != nil {
+					return err
+				}
+
+				authority, err := shared.LoadSubordinate(s.Name)
+				if err != nil {
+					return err
+				}
+				publish_files(authority, filepath.Join("..", dest))
+			}
+
+			fmt.Println(color.Warn(fmt.Sprintf("~~~~~~\nThis certificate authority has been published to '%s'.", dest)))
+
+			return nil
+		},
+	}
+
+	cmd.Flags().String("destination", ".publish", "directory to publish to")
+	cmd.Flags().BoolP("force", "f", false, "overwrite existing published files")
+
+	return cmd
+}
---
+++ cmd/publish/publish_files.go
@@ -0,0 +1,109 @@
+/*
+Copyright © 2026 Julian Easterling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package publish
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/dcjulian29/cert-auth/internal/shared"
+	"github.com/dcjulian29/go-toolbox/color"
+	"github.com/dcjulian29/go-toolbox/execute"
+	"github.com/dcjulian29/go-toolbox/filesystem"
+)
+
+func publish_files(authority shared.Authority, dest string) error {
+	fmt.Println(color.Info(fmt.Sprintf("Authority '%s' is valid. Proceeding to publish...", authority.Name)))
+
+	if authority.Type != "root" {
+		if err := os.Chdir(authority.Name); err != nil {
+			return err
+		}
+	}
+
+	fmt.Println(color.Info(fmt.Sprintf(">>>---------------- '%s' Certificate Authority", authority.Name)))
+
+	if authority.Type == "imported" {
+		fmt.Printf("\033[1;36mThis is an imported authority.\033[0m\n")
+	} else {
+		pass, err := shared.AskPrivateKeyPassword()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Updating the certificate authority database...")
+		if err := shared.UpdateAuthority(pass); err != nil {
+			return err
+		}
+
+		if err := shared.UpdateCRL(pass); err != nil {
+			return err
+		}
+	}
+
+	if filesystem.FileExists(filepath.Join("certs", "ca.pem")) {
+		if err := execute.ExternalProgram("openssl", []string{
+			"x509",
+			"-outform der",
+			"-in certs/ca.pem",
+			fmt.Sprintf("-out %s/%s.crt", dest, authority.Name),
+		}...); err != nil {
+			return err
+		}
+	}
+
+	if filesystem.FileExists("ca.crl") {
+		if err := execute.ExternalProgram("openssl", []string{
+			"crl",
+			"-in ca.crl",
+			fmt.Sprintf("-out %s/%s.crl", dest, authority.CommonName),
+			"-outform der",
+		}...); err != nil {
+			return err
+		}
+	}
+
+	if filesystem.FileExists(filepath.Join("certs", "ocsp.pem")) {
+		if err := execute.ExternalProgram("openssl", []string{
+			"x509",
+			"-outform der",
+			"-in certs/ocsp.pem",
+			fmt.Sprintf("-out %s/%s-ocsp.crt", dest, authority.CommonName),
+		}...); err != nil {
+			return err
+		}
+	}
+
+	if filesystem.FileExists(filepath.Join("certs", "timestamp.pem")) {
+		if err := execute.ExternalProgram("openssl", []string{
+			"x509",
+			"-outform der",
+			"-in certs/ocsp.pem",
+			fmt.Sprintf("-out %s/%s-timestamp.crt", dest, authority.CommonName),
+		}...); err != nil {
+			return err
+		}
+	}
+
+	if authority.Type != "root" {
+		if err := os.Chdir("../"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
